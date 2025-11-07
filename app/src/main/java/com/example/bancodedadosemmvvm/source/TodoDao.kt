12c637255dc7d3e5474ad5746ea7e58846a4e9af
higorsenacosa <higor.vmorais@senacsp.edu.br>
2025-11-07 10:59:11 -0300

    Aula atualizada

--- app/src/main/java/com/example/bancodedadosemmvvm/source/TodoDao.kt
+++ app/src/main/java/com/example/bancodedadosemmvvm/source/TodoDao.kt
@@ -1,6 +1,8 @@
 package com.example.bancodedadosemmvvm.source
 import androidx.room.*
 import com.example.bancodedadosemmvvm.model.Todo
+
+// colocar o Dao
 interface TodoDao {
     @Insert
     suspend fun addItem(todo: Todo)
@@ -8,7 +10,7 @@ interface TodoDao {
     @Query("SELECT * FROM Todo ORDER BY completou, id")
     suspend fun obterElementos(): List<Todo>
 
-    @Query("UPDATE Todo SET completou = 1 WHERE id = id")
+    @Query("UPDATE Todo SET completou = 1 WHERE id = id") // Diferenciar o id interno do externo :id - externo
     suspend fun marcarComoConcluido(id: Int)
 
     @Delete
