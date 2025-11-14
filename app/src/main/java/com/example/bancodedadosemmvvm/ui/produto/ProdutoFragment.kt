2b5884a4dd97d557fa2706ead478298bd767298e
higorsenacosa <higor.vmorais@senacsp.edu.br>
2025-11-14 11:22:45 -0300

    Aula atualizada

--- app/src/main/java/com/example/bancodedadosemmvvm/ui/produto/ProdutoFragment.kt
+++ app/src/main/java/com/example/bancodedadosemmvvm/ui/produto/ProdutoFragment.kt
@@ -1,27 +1,39 @@
 package com.example.bancodedadosemmvvm.ui.produto
 
+import android.app.AlertDialog
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.example.bancodedadosemmvvm.R
-
-class ProdutoFragment : Fragment() {
-
+import com.example.bancodedadosemmvvm.ViewModelFactory
+import com.example.bancodedadosemmvvm.model.Todo
+import com.example.bancodedadosemmvvm.repository.TodoRepository
+import com.example.bancodedadosemmvvm.repository.TodoRepositoryImplementacao
+import com.example.bancodedadosemmvvm.source.AppDatabase
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+class ProdutoFragment : Fragment(), TodoAdapter.AdaptandoLista {
     companion object {
         fun newInstance() = ProdutoFragment()
     }
+    private lateinit var mainViewModel: ProdutoViewModel
+    private lateinit var adaptando: TodoAdapter
+    private lateinit var todoRepositoryImpl: TodoRepository
 
-    private val viewModel: ProdutoViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    lateinit var bancodedados : AppDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,4 +42,99 @@ class ProdutoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_produto, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        iniciarBanco()
+        todoRepositoryImpl =
+            TodoRepositoryImplementacao(
+                bancodedados.todoDao()
+            )
+        val factory = ViewModelFactory(todoRepositoryImpl)
+        mainViewModel = ViewModelProvider(this, factory).
+                get(ProdutoViewModel::class.java)
+        val fabAdd= view?.findViewById<FloatingActionButton>(R.id.fabAdd)
+        configLista()
+        observar()
+        fabAdd?.setOnClickListener { abrirNovoElemento() }
+    }
+    private fun iniciarBanco(){
+        bancodedados = context.let{
+            AppDatabase.invoke(it!!)
+        }
+    }
+    private fun configLista(){
+        adaptando = TodoAdapter(mutableListOf(), this,requireContext())
+        val rvLista = view?.findViewById<RecyclerView>(R.id.rvLista)
+        rvLista?.layoutManager = LinearLayoutManager(context)
+        rvLista?.adapter = adaptando
+    }
+    private fun observar(){
+        mainViewModel.obterTodos()
+        mainViewModel.observeTodos().observe(viewLifecycleOwner, Observer{
+            adaptando.updateList(it as MutableList<Todo>)
+        })
+        mainViewModel.observeErros().observe(viewLifecycleOwner, Observer{
+            if (it != null){
+                val txtErro = view?.findViewById<TextView>(R.id.txtErro)
+                txtErro?.visibility = VISIBLE
+                val rvLista = view?.findViewById<RecyclerView>(R.id.rvLista)
+                rvLista?.visibility = GONE
+                val fabAdd = view?.findViewById<FloatingActionButton>(R.id.fabAdd)
+                fabAdd?.visibility = GONE
+            }
+        })
+    }
+    override fun deletarItem(todo: Todo){
+        abrirDialogo(todo)
+    }
+    private fun abrirDialogo(todo: Todo){
+        val dialogo = AlertDialog.Builder(requireContext())
+        dialogo.setTitle("Removendo elemento")
+        dialogo.setMessage("Você deseja remover esse item?")
+        dialogo.setPositiveButton("Deletar"){_,_ -> mainViewModel.deletarItem(todo)}
+        dialogo.setNegativeButton("Cancelar"){_,_ -> null}
+        dialogo.show()
+    }
+    private fun abrirNovoElemento(){
+        val dialogo = AlertDialog.Builder(requireContext())
+        dialogo.setTitle("Adicionar novo produto")
+
+        val layout = LinearLayout(context)
+        layout.orientation = LinearLayout.VERTICAL
+        val lp = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        lp.setMargins(32,24,32,0)
+
+        val nome = EditText(context)
+        nome.hint = "Digite o nome do produto"
+        nome.layoutParams = lp
+        layout.addView(nome)
+
+        val imagem = EditText(context)
+        imagem.hint = "Digite o link da imagem"
+        imagem.layoutParams = lp
+        layout.addView(imagem)
+
+        val preco = EditText(context)
+        preco.hint = "Digite o preço do produto"
+        preco.inputType = android.text.InputType.TYPE_CLASS_NUMBER or
+                android.text.InputType.TYPE_NUMBER_FLAG_DECIMAL
+        preco.layoutParams = lp
+        layout.addView(preco)
+        dialogo.setView(layout)
+        dialogo.setPositiveButton("Salvar"){_,_, ->
+            val nomeProduto = nome.text.toString()
+            val imagemProduto = imagem.text.toString()
+            val precoProduto = preco.text.toString().toDoubleOrNull() ?: 0.0
+            mainViewModel.InserirItem(nomeProduto,precoProduto,imagemProduto)
+        }
+        dialogo.setNegativeButton("Cancelar", null)
+        dialogo.show()
+    }
+
+    override fun marcarComoConcluido(todo: Todo) {
+        mainViewModel.atualizarItem(todo)
+    }
 }
--- app/src/main/java/com/example/bancodedadosemmvvm/ui/produto/TodoAdapter.kt
+++ app/src/main/java/com/example/bancodedadosemmvvm/ui/produto/TodoAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.bancodedadosemmvvm.ui.produto
 
 import android.content.Context
+import android.provider.DocumentsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,8 +15,70 @@ import com.example.bancodedadosemmvvm.model.Todo
 
 class TodoAdapter(
     private val itemlista: MutableList<Todo>,
-    private val listando: AdapterListener,
+    private val listando: AdaptandoLista,
     private val contexto : Context
 ) : RecyclerView.Adapter<TodoAdapter.ViewHolder>(){
-
+    override fun getItemCount() = itemlista.size
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int): ViewHolder {
+        return ViewHolder(parent.
+        inflate(R.layout.item_a_ser_listado))
+    }
+    override fun onBindViewHolder(
+        holder: TodoAdapter.ViewHolder,
+        position: Int) {
+        holder.bindViewHolder(itemlista[position])
+    }
+    fun updateList(itemlista: MutableList<Todo>){
+        this.itemlista.clear()
+        this.itemlista.addAll(itemlista)
+        notifyDataSetChanged()
+    }
+    interface AdaptandoLista{
+        fun marcarComoConcluido(todo: Todo)
+        fun deletarItem(todo: Todo)
+    }
+    inner class ViewHolder(itemView: View):
+            RecyclerView.ViewHolder(itemView){
+                private lateinit var item: Todo
+                fun bindViewHolder(item: Todo){
+                    this.item = item
+            val txtTarefa = itemView.findViewById<TextView>(R.id.txtTarefa)
+                    txtTarefa.text = item.nome
+                    configTodosCompletos(item)
+                val img = itemView.findViewById<ImageView>(R.id.imgCompletou)
+                img.setOnClickListener { listando.marcarComoConcluido(item) }
+                    //Criar um botão remover
+                val elemento = itemView.findViewById<LinearLayout>(R.id.item_lista_controlador)
+                    elemento.setOnClickListener {
+                        listando.deletarItem(item)
+                        return@setOnClickListener
+                    }
+                }
+        private fun configTodosCompletos(todo: Todo){
+                if (todo.completou){
+                    DrawableCompat.setTint(
+                        DrawableCompat.wrap(itemView.
+                            findViewById<ImageView>(R.id.imgCompletou).
+                                drawable),
+                        ContextCompat.getColor(contexto,R.color.black)
+                    )
+                }
+                else{
+                    DrawableCompat.setTint(
+                        DrawableCompat.wrap(itemView.
+                        findViewById<ImageView>(R.id.imgCompletou).
+                        drawable),
+                        ContextCompat.getColor(contexto,R.color.teal_200)
+                    )
+                }
+            }
+        }
+}
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int,
+                      attachToRoot: Boolean = false): View{
+    return LayoutInflater.from(context).inflate(layoutRes,
+        this,
+        attachToRoot)
 }
